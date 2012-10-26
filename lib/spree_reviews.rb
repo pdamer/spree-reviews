19bbc49bc1493a40e5f13dfad2d76870c878b4bb
Paul Damer <pdamer@gmail.com>
2012-10-26 10:58:08 -0400

    loading overrides

--- lib/spree_reviews.rb
+++ lib/spree_reviews.rb
@@ -1,5 +1,4 @@
 require 'spree_core'
-require 'spree_reviews_hooks'
 
 module SpreeReviews
   
@@ -24,6 +23,9 @@ module SpreeReviews
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
+      end
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
       end
       ProductsHelper.send(:include, ReviewsHelper)
       Admin::ReviewsController.cache_sweeper :review_sweeper
